0abd8f29810c1964bc4cdc5c486d1f4ee7890a25
Devin Catuns <devincatuns1@gmail.com>
2026-04-11 21:05:51 -0400

    enable service discovery client

--- src/main/java/xyz/catuns/edupulse/profile/ProfileServiceApplication.java
+++ src/main/java/xyz/catuns/edupulse/profile/ProfileServiceApplication.java
@@ -2,10 +2,12 @@ package xyz.catuns.edupulse.profile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import xyz.catuns.spring.jwt.autoconfigure.annotation.EnableJwtSecurity;
 
 @EnableJwtSecurity
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ProfileServiceApplication {
 
